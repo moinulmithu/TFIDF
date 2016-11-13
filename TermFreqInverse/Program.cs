e2eb36fe206059515b0ace1ca66c515c976243ca
Moinul Islam <moinulmithu@hotmail.com>
2016-11-13 23:46:01 +0600

    Init for TF*IDF

--- TermFreqInverse/Program.cs
+++ TermFreqInverse/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,87 @@ namespace TermFreqInverse
     {
         static void Main(string[] args)
         {
-        }
-    }
+            // Read a file into a string (this file must live in the same directory as the executable)
+            //string filename = "TFIDF.txt";
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filename = filePath+ @"\TFIDF.txt";
+            string inputString = File.ReadAllText(filename);
+
+            // Convert our input to lowercase
+            inputString = inputString.ToLower();
+
+            // Define characters to strip from the input and do it
+            string[] stripChars = { ";", ",", ".", "-", "_", "^", "(", ")", "[", "]",
+                        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\n", "\t", "\r" };
+            foreach (string character in stripChars)
+            {
+                inputString = inputString.Replace(character, "");
+            }
+
+            // Split on spaces into a List of strings
+            List<string> wordList = inputString.Split(' ').ToList();
+
+            // Define and remove stopwords
+            string[] stopwords = new string[] { "and", "the", "she", "for", "this", "you", "but" };
+            foreach (string word in stopwords)
+            {              
+                while (wordList.Contains(word))
+                {
+                    wordList.Remove(word);
+                }
+            }
+
+            // Create a new Dictionary object
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            // Loop over all over the words in our wordList...
+            foreach (string word in wordList)
+            {
+                // If the length of the word is at least three letters...
+                if (word.Length >= 3)
+                {
+                    // ...check if the dictionary already has the word.
+                    if (dictionary.ContainsKey(word))
+                    {
+                        // If we already have the word in the dictionary, increment the count of how many times it appears
+                        dictionary[word]++;
+                    }
+                    else
+                    {
+                        // Otherwise, if it's a new word then add it to the dictionary with an initial count of 1
+                        dictionary[word] = 1;
+                    }
+
+                } // End of word length check
+
+            } // End of loop over each word in our input
+
+            // Create a dictionary sorted by value (i.e. how many times a word occurs)
+            var sortedDict = (from entry in dictionary orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            // Loop through the sorted dictionary and output the top 10 most frequently occurring words
+            int count = 1;
+            Console.WriteLine("---- Most Frequent Terms in the File: " + filename + " ----");
+            Console.WriteLine();
+            foreach (KeyValuePair<string, int> pair in sortedDict)
+            {
+                // Output the most frequently occurring words and the associated word counts
+                Console.WriteLine(count + "\t" + pair.Key + "\t" + pair.Value);
+                count++;
+
+                // Only display the top 10 words then break out of the loop!
+                if (count > 10)
+                {
+                    break;
+                }
+            }
+
+            // Wait for the user to press a key before exiting
+            Console.ReadKey();
+
+        } // End of Main method
+
+    } // End of Program class
+
 }
+
